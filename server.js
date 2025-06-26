766fc78404aa892d294cfc529aa04bf0c8a6446b
danish04dz <mohd.692003@gmail.com>
2025-06-26 14:53:49 +0530

    DAY 2

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log("Day 1")
+const express= require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require ('cors');
+
+dotenv.config();
+
+const app =express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("PrintYatri API is Running");
+});
+
+// MongoDb Connection
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log("DB Connected Successfully");
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`server is running at http://localhost:${process.env.PORT}`)
+
+    })
+})
+.catch((err)=>
+    console.error(err)
+)
